ddbe13a03c0893d9874555e076daa6e0f6e3d33a
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-29 14:12:57 -0800

    streamlined code

--- clock.js
+++ clock.js
@@ -1,30 +1,9 @@
-//variable declare
-var domSecond = document.getElementById("second");
-var domMinute = document.getElementById("minute");
-var domHour = document.getElementById("hour");
-var curTime;
-var curSecond;
-var curMinute;
-var curHour;
-//pass current time and type and return degree change
-function findPosition(num, type){
-	if(type === "second" || type ==="minute"){
-		return(num * 6);
-	}else{
-		return(num * 30);
-	}
-}
-//Run Interval, annon func to get times and set positions
 setInterval(function(){
-	curTime = new Date();
-	curSecond = curTime.getSeconds();
-	curMinute = curTime.getMinutes();
-	curHour = curTime.getHours();
-	//change hour to 12-hour formate
-	if(curHour > 12){
-		curHour -= 12;
-	}
-	domSecond.style.transform = "rotate("+findPosition(curSecond, "second")+"deg)";
-	domMinute.style.transform = "rotate("+findPosition(curMinute, "minute")+"deg)";
-	domHour.style.transform = "rotate("+findPosition(curHour, "hour")+"deg)";	
+	var curTime = new Date();
+	var curSecond = curTime.getSeconds();
+	var curMinute = curTime.getMinutes();
+	var curHour = curTime.getHours() > 12 ? curTime.getHours() - 12 : curTime.getHours();
+	document.getElementById("second").style.transform = "rotate("+ (curSecond*6)+"deg)";
+	document.getElementById("minute").style.transform = "rotate("+ (curMinute*6)+"deg)";
+	document.getElementById("hour").style.transform = "rotate("+ (curHour*30) + "deg)";	
 }, 1000);
